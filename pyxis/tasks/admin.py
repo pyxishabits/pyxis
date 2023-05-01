c49fdbbd5d1c52bf74285e6949fb36147341261f
LiamDes <deshaiesash@gmail.com>
2023-05-01 12:22:32 -0700

    inital models

--- pyxis/tasks/admin.py
+++ pyxis/tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Habit, HabitTask, Task, Journal
 
 # Register your models here.
+admin.site.register(Habit)
+admin.site.register(HabitTask)
+admin.site.register(Task)
+admin.site.register(Journal)
