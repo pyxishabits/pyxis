6d9b8770da8dba66170b30935553cea11b01dc75
Cynthia S <c.sovulewski@gmail.com>
2023-05-01 14:15:02 -0700

    requested changes fixed

--- pyxis/pyxis/settings.py
+++ pyxis/pyxis/settings.py
@@ -125,8 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# blank until we decide where to redirect
+
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
-# LOGIN_URL = 'login'
