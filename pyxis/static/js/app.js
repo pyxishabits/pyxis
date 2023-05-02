8f2a7c13bd50058292876175185f64cde00a1942
LiamDes <deshaiesash@gmail.com>
2023-05-02 14:32:09 -0700

    create reactive classes for weekday and primary focus display

--- pyxis/static/js/app.js
+++ pyxis/static/js/app.js
@@ -4,6 +4,7 @@ new Vue({
     data: {
         token: '',
         habits: {},
+        habitsPreview: {},
         tasks: {},
         journalEntries: {},
         daysOfTheWeek: {
@@ -14,14 +15,50 @@ new Vue({
             4: { name: 'thursday', abbrv: 'T' },
             5: { name: 'friday', abbrv: 'F' },
             6: { name: 'saturday', abbrv: 'S' },
-        }
+        },
+        today: '',
+        activeHabits: false,
+        activeTasks: false,
+        activeJournal: false,
     },
     methods: {
         getHabits() {
-            axios.get('api/habits')
-                .then(response => {
-                    this.habits = response.data
-                })
+            // axios.get('api/habits')
+            //     .then(response => {
+            //         this.habits = response.data
+            //     })
+            this.habits = [
+                {name: 'Walk dog',
+                description: 'needs to be done!',
+                recurrence: '[0,2,3,4]',
+                user: 'liamdes'},
+                {name: 'Make Breakfast',
+                description: 'eat every day',
+                recurrence: '[0,1,2,3,4,5,6]',
+                user: 'liamdes'},
+                {name: 'Take Nap',
+                description: 'catch up on sleep :)',
+                recurrence: '[1,3,5]',
+                user: 'liamdes'},
+                {name: 'Exercise',
+                description: 'doesn\'t need to be done that often',
+                recurrence: '[0]',
+                user: 'liamdes'},
+                {name: 'Grocery Shop',
+                description: 'cannot do a lot without this',
+                recurrence: '[6]',
+                user: 'liamdes'},
+            ]
+        },
+        previewHabit() {
+            // retrieve just first X number of habits for the day
+            // if len of habits exceeds X, add a "More..." to end of list
+            this.habitsPreview = [
+                {name: 'Walk Dog'},
+                {name: 'Make Breakfast'},
+                {name: 'Take Nap'},
+                {name: 'More...'}
+            ]
         },
         editHabit() {
 
@@ -31,17 +68,25 @@ new Vue({
         },
         getJournals() {
 
+        },
+        isToday(day) {
+            if (day.name === this.today) {
+                    return true
+            } else {
+                    return false
+            }
         }
     },
     computed: {
-        weekday() {
+        todayWeekday() {
             const todayDate = new Date
-            const today = todayDate.getDate()
-            return this.currentWeekday = today
-        }
+            const todayIndex = todayDate.getDate()
+            return this.today = this.daysOfTheWeek[todayIndex].name
+        },
     },
     mounted() {
+        this.previewHabit()
         this.getHabits()
         this.token = document.querySelector('input[name=csrfmiddlewaretoken]').value
-    }
+    },
 })
