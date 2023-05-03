7a55a649b2fa318136085f6ced96031a66156779
Cynthia S <121696912+csovu@users.noreply.github.com>
2023-05-03 10:27:12 -0700

    Added URL endpoints for Tasks (#6)
    * initual vue setup
    
    * revert changes to home.html
    
    * update task added
    
    * added api endpoints and api test calls
    
    * cleanup

--- pyxis/api/urls.py
+++ pyxis/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from .views import *
 from . import views
 
@@ -6,7 +6,10 @@ appname = 'apis'
 urlpatterns = [
     path('habits/', HabitView.as_view()), #all habits
     path('habits/<int:pk>/', HabitDetail.as_view()), #CRUD
-    path('tasks/', TaskView.as_view()),
+    path('tasks/<int:pk>/', TaskView.as_view()),
+    path('tasks/', TaskToday.as_view()),
+    path('tasks/new/', CreateTask.as_view()),
+    path('tasks/<int:pk>/done/', DoneTask.as_view()),
     path('journal/', JournalView.as_view()),
     path('', views.current_user, name='current_user'), 
 ]
