eb9f76d07eec3e9a28ed17830906e0d5c0edb747
Liam D <104411615+LiamDes@users.noreply.github.com>
2023-05-04 11:58:16 -0700

    Frontend consume APIs for Tasks (#9)
    * add buttons to habits/tasks, toggle completion of task
    
    * create tasks and maintain active tab space
    
    * reverse chron order on tasks, change display toggle to header only
    
    * remove tasks, reorganize all headers
    
    * toggle or save for edit task name and desc
    
    * bind edit description
    
    * add display of urgency/importance and due date if applicable
    
    * preserve whitespace on descrip
    
    * review notes
    
    * adjust date reading

--- pyxis/static/js/app.js
+++ pyxis/static/js/app.js
@@ -3,6 +3,7 @@ new Vue({
     delimiters: ['[[', ']]'],
     data: {
         token: '',
+        currentUser: '',
         habits: {},
         habitsPreview: {},
         tasks: {},
@@ -23,46 +24,27 @@ new Vue({
         changeWeekNext: false,
         changeWeekPrev: false,
         weekStart: '',
-        weekEnd: ''
+        weekEnd: '',
+        addTaskWindow: false,
+        newTaskName: '',
+        newTaskDesc: '',
+        newTaskUrgent: false,
+        newTaskImportant: false,
+        newTaskDue: null,
     },
     methods: {
+        getUser() {
+            axios.get('/api/').then(res => this.currentUser = res.data.id)
+        },
         getHabits() {
             // axios.get('api/habits')
             //     .then(response => {
             //         this.habits = response.data
             //     })
-            this.habits = [
-                {name: 'Walk dog',
-                description: 'needs to be done!',
-                recurrence: '[0,2,3,4]',
-                user: 'liamdes'},
-                {name: 'Make Breakfast',
-                description: 'eat every day',
-                recurrence: '[0,1,2,3,4,5,6]',
-                user: 'liamdes'},
-                {name: 'Take Nap',
-                description: 'catch up on sleep :)',
-                recurrence: '[1,3,5]',
-                user: 'liamdes'},
-                {name: 'Exercise',
-                description: 'doesn\'t need to be done that often',
-                recurrence: '[0]',
-                user: 'liamdes'},
-                {name: 'Grocery Shop',
-                description: 'cannot do a lot without this',
-                recurrence: '[6]',
-                user: 'liamdes'},
-            ]
         },
         previewHabit() {
             // retrieve just first X number of habits for the day
             // if len of habits exceeds X, add a "More..." to end of list
-            this.habitsPreview = [
-                {name: 'Walk Dog'},
-                {name: 'Make Breakfast'},
-                {name: 'Take Nap'},
-                {name: 'More...'}
-            ]
         },
         editHabit() {
 
@@ -70,19 +52,45 @@ new Vue({
         getTodayTasks() {
             axios.get('api/tasks/')
             .then(response => {
-                this.tasks = response.data
+                this.tasks = response.data.reverse()
             })
-
         },
-        // TODO: pass task object in as param
-        updateTask() {
-            axios.patch(`/api/tasks/1/done/`,{},  
+        updateTask(taskID) {
+            axios.patch(`api/tasks/${taskID}/done/`,{},  
                 { headers: {'X-CSRFToken': this.token }}
             ).then(() => this.getTodayTasks())
         },
-
+        deleteTask(taskID) {
+            axios.delete(`api/tasks/${taskID}/`, {
+            headers: {'X-CSRFToken': this.token }}).then(() => this.getTodayTasks())
+        },
+        addTask() {
+            axios.post(`api/tasks/new/`, {
+                "name": this.newTaskName,
+                "description": this.newTaskDesc,
+                "date": this.newDate(),
+                "completed_time": null,
+                "is_urgent": this.newTaskUrgent,
+                "is_important": this.newTaskImportant,
+                "due_date": this.newTaskDue,
+                "user": this.currentUser
+            }, {headers: {'X-CSRFToken': this.token }})
+            .then(() => this.getTodayTasks())
+            this.addTaskWindow = false,
+            this.newTaskName = '',
+            this.newTaskDesc = '',
+            this.newTaskUrgent = false,
+            this.newTaskImportant = false,
+            this.newTaskDue = null
+        },
+        newDate() {
+            const newDate = new Date()
+            let jsonDate = newDate.toJSON()
+            let dateQuery = jsonDate.slice(0, 10)
+            return dateQuery
+        },
         getJournals() {
-
+            
         },
         weekNext() {
             this.changeWeekNext = true
@@ -116,6 +124,12 @@ new Vue({
             this.weekStart = firstday
             this.weekEnd = lastday
         },
+        openTasks() {
+            this.addTaskWindow = !this.addTaskWindow
+            this.activeTasks = true
+            this.activeHabits = false
+            this.activeJournal = false
+        }
     },
     computed: {
         todayWeekday() {
@@ -127,9 +141,114 @@ new Vue({
         },
     },
     mounted() {
-        this.previewHabit()
-        this.getHabits()
+        this.getUser()
+        this.getJournals()
+        this.getTodayTasks()
         this.getWeek()
         this.token = document.querySelector('input[name=csrfmiddlewaretoken]').value
     },
+})
+
+
+Vue.component('UserTasks', {
+    template: `
+        <div>
+        <span v-if="!task.completed_time" @click="$emit('update', task.id)">
+            <i class="fa-regular fa-square"></i>
+        </span>
+        <span v-else-if="task.completed_time" @click="$emit('update', task.id)">
+            <i class="fa-solid fa-square-check"></i>
+        </span>
+        <span v-if="editing === null" class="name">[[ task.name ]]</span>
+        <span v-else-if="editing === task.id">
+            <input type="text" v-model="editTaskName" class="editfield">
+        </span>
+        <i class="fa-solid fa-pen-to-square" title="Edit" @click="editToggle"></i>
+        <i class="fa-regular fa-trash-can" title="Delete" @click="$emit('delete', task.id)"></i>
+            <div>
+                <span v-if="editing === null" class="detail descrip">[[ task.description ]]</span>
+                <div v-else-if="editing === task.id">
+                <textarea v-model="editTaskDesc" class="editfield"></textarea>
+                </div>
+            </div>
+            <div>
+                <span v-if="task.is_urgent">
+                    <span v-if="editing === null" class="detail">
+                        URGENT
+                    </span>
+                </span>
+                <span v-if="editing === task.id">
+                    Urgent: <input type="checkbox" v-model="editTaskUrgent">
+                </span>
+                <span v-if="task.is_important">
+                    <span v-if="editing === null" class="detail">
+                        IMPORTANT
+                    </span>
+                </span>
+                <span v-if="editing === task.id">
+                    Important: <input type="checkbox" v-model="editTaskImportant">
+                </span>
+            </div>
+            <div v-if="task.due_date">
+                <span v-if="editing === null" class="detail">Due: [[ dueDisplay ]]</span>
+                <span v-else-if="editing === task.id">
+                    <input type="date" v-model="editTaskDue" class="editfield">
+                </span>
+            </div> 
+            <button v-if="editing === task.id" @click="editTask" class="save">
+                <i class="fa-solid fa-floppy-disk"></i>
+            </button>
+        </div>
+        `,
+    props: {
+        task: Object,
+    },
+    delimiters: ['[[', ']]'],
+    data: () => {
+        return {
+            editing: null,
+            editTaskName: '',
+            editTaskDesc: '',
+            dueDisplay: '',
+            editTaskDue: null,
+            editTaskUrgent: false,
+            editTaskImportant: false,
+        }
+    },
+    methods: {
+        editTask() {
+            axios.patch(`api/tasks/${this.task.id}/`, {
+                "name": this.editTaskName,
+                "description": this.editTaskDesc,
+                "due_date": this.editTaskDue,
+                "is_urgent": this.editTaskUrgent,
+                "is_important": this.editTaskImportant
+            },  
+                { headers: {'X-CSRFToken': this.$parent.token }}
+            ).then(() => { this.$parent.getTodayTasks() })
+            this.editing = null
+        },
+        editToggle() {
+            if (this.editing === null) {
+                return this.editing = this.task.id
+            } else { return this.editing = null }
+        },
+        dueDate() {
+            const newDate = new Date(this.task.due_date)
+            let jsonDate = newDate.toJSON()
+            let dateQuery = jsonDate.slice(0, 10)
+            this.dueDisplay = dateQuery
+        },
+    },
+    mounted() {
+        this.dueDate()
+        this.editTaskName = this.task.name
+        this.editTaskDesc = this.task.description
+        this.editTaskDue = this.dueDate()
+        this.editTaskUrgent = this.task.is_urgent
+        this.editTaskImportant = this.task.is_important
+    },
+    updated() {
+        this.dueDate()
+    }
 })
