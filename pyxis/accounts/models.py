af20180605864265f39c2c13e52b399cfb22da6f
Cynthia S <c.sovulewski@gmail.com>
2023-05-09 09:44:29 -0700

    updated custom user to include colortheme

--- pyxis/accounts/models.py
+++ pyxis/accounts/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.utils import timezone
-
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    COLOR_OPTIONS = (('D','dark'),('L','light'))
     timezone = models.CharField(max_length=40, null=True, blank=True)
-
+    color_theme = models.CharField(choices=COLOR_OPTIONS, max_length=1, default='L')
     def __str__(self):
         return self.username
+    
