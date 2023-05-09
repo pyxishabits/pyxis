adb7064bc07c602ac7830809612a58f781ee2b05
Cynthia S <c.sovulewski@gmail.com>
2023-05-09 10:49:19 -0700

    updated userserializer param

--- pyxis/api/serializers.py
+++ pyxis/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from tasks.models import Task, Habit, HabitTask, Journal
-
+from django.contrib.auth import get_user_model
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
@@ -40,5 +40,5 @@ class JournalSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'id']
