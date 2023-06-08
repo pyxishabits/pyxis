4d4ab941b972e6bdf52bd16d368227204155346b
Danny Burrow <danny@pdxcodeguild.com>
2023-06-08 12:04:07 -0700

    remove client

--- pyxis/pyxis/urls.py
+++ pyxis/pyxis/urls.py
@@ -25,7 +25,4 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
     path('accounts/', include('accounts.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
-    path('tz_detect/', include('tz_detect.urls')),
 ]
